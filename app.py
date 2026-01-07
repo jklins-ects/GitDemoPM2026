e2c25d5109c02ea0e00f3ea2892d5bfe434e1c1b
jklins-ects <73347118+jklins-ects@users.noreply.github.com>
2026-01-07 12:49:29 -0500

    force a merge

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 print("Git/Github Demo")
 # some comment
-print("Hello from the local repo")
-print("Hello from the remote repo")
+print("Hello from the local repo 0")
+print("Hello from the remote repo 1")
+print("Hey remote, you a dummy!")
